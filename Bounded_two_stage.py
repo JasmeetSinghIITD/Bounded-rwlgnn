16633db0c7092e52652e6cceeb91ee309ec43304
SARATH MOHAN <86975877+SarathMohanIITD@users.noreply.github.com>
2023-03-19 03:37:13 +0530

    Merge remote-tracking branch 'origin/main'

--- Bounded_two_stage.py
+++ Bounded_two_stage.py
@@ -66,8 +66,8 @@ class RwlGNN:
         # INIT
         # self.weight = torch.rand(int(n*(n-1)/2),dtype=torch.float,requires_grad=True,device = self.device)
         self.weight = self.Linv(L_noise)         ###################################################
-        self.w_old= self.Linv(L_noise)
-        #self.w_old = torch.zeros_like(self.weight)  ####################  To store previous w value ( w^{t-1} )
+        #self.w_old= self.Linv(L_noise)
+        self.w_old = torch.ones_like(self.weight)  ####################  To store previous w value ( w^{t-1} )
 
 
         self.weight.requires_grad = True
