cabd0b040db7de982be1b5ef45bd468b7fe3a3d7
SARATH MOHAN <86975877+SarathMohanIITD@users.noreply.github.com>
2023-03-19 02:46:54 +0530

    Merge remote-tracking branch 'origin/main'

--- bounded_gcn.py
+++ bounded_gcn.py
@@ -225,8 +225,7 @@ class BoundedGCN(nn.Module):
             optimizer.zero_grad()
             output = self.forward(self.features, self.adj_norm)
 
-            self.l2_reg = self.bound * torch.square(torch.norm(self.gc1.weight)) \
-                          + torch.square(torch.norm(self.gc2.weight))     # Added by me
+            self.l2_reg = self.bound * (torch.square(torch.norm(self.gc1.weight)) + torch.square(torch.norm(self.gc2.weight)) )    # Added by me
 
             loss_train = F.nll_loss(output[idx_train], labels[idx_train]) + self.bound*self.l2_reg
 
